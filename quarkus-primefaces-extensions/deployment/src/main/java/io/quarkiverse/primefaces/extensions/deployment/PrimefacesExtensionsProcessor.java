77c90b50a6ad94a0560436a81d470cfff44eccc3
melloware <mellowaredev@gmail.com>
2023-02-18 11:10:07 -0500

    Formatted

--- quarkus-primefaces-extensions/deployment/src/main/java/io/quarkiverse/primefaces/extensions/deployment/PrimefacesExtensionsProcessor.java
+++ quarkus-primefaces-extensions/deployment/src/main/java/io/quarkiverse/primefaces/extensions/deployment/PrimefacesExtensionsProcessor.java
@@ -20,7 +20,6 @@ import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuil
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.primefaces.extensions.runtime.PrimeFacesExtensionsFeature;
 import io.quarkus.primefaces.extensions.runtime.PrimeFacesExtensionsRecorder;
-import io.quarkus.primefaces.runtime.PrimeFacesRecorder;
 
 class PrimefacesExtensionsProcessor {
 
@@ -42,7 +41,8 @@ class PrimefacesExtensionsProcessor {
         index.produce(new IndexDependencyBuildItem("org.primefaces.extensions", "resources-ckeditor"));
         index.produce(new IndexDependencyBuildItem("com.google.code.gson", "gson"));
         index.produce(new IndexDependencyBuildItem("com.googlecode.libphonenumber", "libphonenumber"));
-        index.produce(new IndexDependencyBuildItem("com.googlecode.owasp-java-html-sanitizer", "owasp-java-html-sanitizer"));
+        index.produce(new IndexDependencyBuildItem("com.googlecode.owasp-java-html-sanitizer",
+                "owasp-java-html-sanitizer"));
         index.produce(new IndexDependencyBuildItem("dev.morphia.morphia", "morphia-core"));
         index.produce(new IndexDependencyBuildItem("org.commonmark", "commonmark"));
     }
@@ -69,15 +69,18 @@ class PrimefacesExtensionsProcessor {
 
     @BuildStep
     @Record(ExecutionTime.STATIC_INIT)
-    void registerForReflection(PrimeFacesExtensionsRecorder recorder, BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+    void registerForReflection(PrimeFacesExtensionsRecorder recorder,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
             CombinedIndexBuildItem combinedIndex) {
         final List<String> classNames = new ArrayList<>();
         // All utilities
         classNames.addAll(
-                collectClassesInPackage(combinedIndex, org.primefaces.extensions.util.Constants.class.getPackageName()));
+                collectClassesInPackage(combinedIndex,
+                        org.primefaces.extensions.util.Constants.class.getPackageName()));
 
         // methods
-        reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, classNames.toArray(new String[classNames.size()])));
+        reflectiveClass.produce(
+                new ReflectiveClassBuildItem(true, false, classNames.toArray(new String[classNames.size()])));
 
         // neither
         reflectiveClass.produce(new ReflectiveClassBuildItem(false, false,
