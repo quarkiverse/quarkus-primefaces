0d8fa3b9522cd10a3471296cc3978377b98d22df
melloware <mellowaredev@gmail.com>
2024-04-19 16:07:18 -0400

    More PF14 fixes

--- quarkus-primefaces/runtime/src/main/java/io/quarkus/primefaces/runtime/PrimeFacesFeature.java
+++ quarkus-primefaces/runtime/src/main/java/io/quarkus/primefaces/runtime/PrimeFacesFeature.java
@@ -3,8 +3,6 @@ package io.quarkus.primefaces.runtime;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 import org.krysalis.barcode4j.output.bitmap.BitmapEncoderRegistry;
-import org.primefaces.component.datatable.export.DataTablePDFExporter;
-import org.primefaces.component.treetable.export.TreeTablePDFExporter;
 
 public class PrimeFacesFeature implements Feature {
     private final static String REASON = "PrimeFaces runtime initialization";
@@ -13,9 +11,8 @@ public class PrimeFacesFeature implements Feature {
     public void afterRegistration(AfterRegistrationAccess access) {
         // Barcode component is optional but must register this for native mode since it uses AWT
         RuntimeClassInitialization.initializeAtRunTime(BitmapEncoderRegistry.class.getName());
-        //Exporters
-        RuntimeClassInitialization.initializeAtRunTime(DataTablePDFExporter.class.getName());
-        RuntimeClassInitialization.initializeAtRunTime(TreeTablePDFExporter.class.getName());
+        // Charts.js uses SecureRandom
+        RuntimeClassInitialization.initializeAtRunTime("software.xdev.chartjs.model.color.Color");
     }
 
     @Override
