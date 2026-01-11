572e155785c6c6304a41fea1bc7eddadbce9ca65
melloware <mellowaredev@gmail.com>
2026-01-11 11:37:13 -0500

    Update DEVUI cards

--- quarkus-primefaces-extensions/deployment/src/main/java/io/quarkiverse/primefaces/extensions/deployment/devui/PrimeFacesExtensionsDevUIProcessor.java
+++ quarkus-primefaces-extensions/deployment/src/main/java/io/quarkiverse/primefaces/extensions/deployment/devui/PrimeFacesExtensionsDevUIProcessor.java
@@ -1,14 +1,9 @@
 package io.quarkiverse.primefaces.extensions.deployment.devui;
 
-import org.primefaces.extensions.util.Constants;
-
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
-import io.quarkus.devui.spi.page.ExternalPageBuilder;
-import io.quarkus.devui.spi.page.Page;
-import io.quarkus.devui.spi.page.PageBuilder;
 
 /**
  * Dev UI card for displaying important details such as the library version.
@@ -18,16 +13,8 @@ public class PrimeFacesExtensionsDevUIProcessor {
     @BuildStep(onlyIf = IsDevelopment.class)
     void createCard(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
         final CardPageBuildItem card = new CardPageBuildItem();
-
-        final PageBuilder<ExternalPageBuilder> versionPage = Page.externalPageBuilder("Version")
-                .icon("font-awesome-solid:book")
-                .url("https://primefaces-extensions.github.io/")
-                .doNotEmbed()
-                .staticLabel(Constants.class.getPackage().getImplementationVersion());
-        card.addPage(versionPage);
-
-        card.setCustomCard("qwc-primefaces-extensions-card.js");
-
+        card.addLibraryVersion("org.primefaces.extensions", "primefaces-extensions", "PrimeFaces Extensions",
+                "https://primefaces-extensions.github.io/");
         cardPageBuildItemBuildProducer.produce(card);
     }
 
--- quarkus-primefaces/deployment/src/main/java/io/quarkiverse/primefaces/deployment/devui/PrimeFacesDevUIProcessor.java
+++ quarkus-primefaces/deployment/src/main/java/io/quarkiverse/primefaces/deployment/devui/PrimeFacesDevUIProcessor.java
@@ -1,14 +1,9 @@
 package io.quarkiverse.primefaces.deployment.devui;
 
-import org.primefaces.util.Constants;
-
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
-import io.quarkus.devui.spi.page.ExternalPageBuilder;
-import io.quarkus.devui.spi.page.Page;
-import io.quarkus.devui.spi.page.PageBuilder;
 
 /**
  * Dev UI card for displaying important details such as the library version.
@@ -18,16 +13,8 @@ public class PrimeFacesDevUIProcessor {
     @BuildStep(onlyIf = IsDevelopment.class)
     void createCard(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
         final CardPageBuildItem card = new CardPageBuildItem();
-
-        final PageBuilder<ExternalPageBuilder> versionPage = Page.externalPageBuilder("Version")
-                .icon("font-awesome-solid:book")
-                .url("https://www.primefaces.org/showcase/")
-                .doNotEmbed()
-                .staticLabel(Constants.class.getPackage().getImplementationVersion());
-        card.addPage(versionPage);
-
-        card.setCustomCard("qwc-primefaces-card.js");
-
+        card.addLibraryVersion("org.primefaces", "primefaces", "PrimeFaces",
+                "https://www.primefaces.org/showcase/");
         cardPageBuildItemBuildProducer.produce(card);
     }
 
